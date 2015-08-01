9273bf2ebf5213364d84d886c09cc61f04fbe0b3
Tyler Machen <tylermachen@gmail.com>
2015-08-01 17:31:25 -0400

    complete bachelor methods

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,28 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  name = nil
+  data[season].each { |c| name = c["name"].split if c["status"] == "Winner" }
+  name[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each { |attribute| return attribute["name"] if attribute["occupation"] == occupation }
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  c = 0
+  data.each do |season, contestants|
+    contestants.each { |contestant| c += 1 if contestant["hometown"] == hometown }
+  end
+  c
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each { |s, c| c.each { |c| return c["occupation"] if c["hometown"] == hometown }}
 end
 
+# testing out how abstract I can make the method; this much abstraction may be too much.
 def get_average_age_for_season(data, season)
-  # code here
+  a = []; data[season].each { |c| a << c["age"].to_f }; (a.reduce(:+) / a.count).round
 end
